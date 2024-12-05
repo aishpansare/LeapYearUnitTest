6102d5bcfa17c1468078983a4a0d0d62a6159007
Aishwarya Shashikant Pansare <Aishwarya@villaplus.com>
2024-12-05 15:15:08 +0530

    updated the test cases

--- LeapYear/LeapYearShould.cs
+++ LeapYear/LeapYearShould.cs
@@ -6,18 +6,37 @@ namespace LeapYear
     public class LeapYearShould
     {
         [Theory]
-        [InlineData(1996, true)] 
-        [InlineData(2001, false)] 
-        [InlineData(1900, false)] 
-        [InlineData(2000, true)]  
-        [InlineData(2024, true)]  
-        [InlineData(2023, false)] 
-        [InlineData(2100, false)] 
-        [InlineData(2400, true)]  
-        public void ReturnsCorrectResult_Years(int year, bool expected)
+        [InlineData(1996)]
+        [InlineData(1992)]
+        public void ReturnTrue_WhenYearIsDivisibleBy4(int year)
         {
-            LeapYear.IsLeapYear(year).Should().Be(expected);
+            LeapYear.IsLeapYear(year).Should().BeTrue();
         }
+
+        [Theory]
+        [InlineData(2001)]
+        [InlineData(2023)]
+        public void ReturnFalse_WhenYearIs_NotDivisibleBy4(int year)
+        {
+            LeapYear.IsLeapYear(year).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(1900)]
+        [InlineData(2100)]
+        public void ReturnFalse_WhenYearIs_NotDivisibleBy4And100_ButNotBy400(int year)
+        {
+            LeapYear.IsLeapYear(year).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData(2000)]
+        [InlineData(2400)]
+        public void ReturnTrue_WhenYearIs_DivisibleBy4And100_ButAlsoBy400(int year)
+        {
+            LeapYear.IsLeapYear(year).Should().BeTrue();
+        }
+
         [Fact]
         public void ThrowsArgumentException_NegativeInput()
         {
